f31119de79e377b1c5f5729b1ba8a4b588d022ae
aboutphilippe <philippemarketing@gmail.com>
2023-07-13 01:15:22 +0200

    test config

---
+++ superset_config.py
@@ -0,0 +1,12 @@
+# Superset specific config
+ROW_LIMIT = 5000
+
+# Flask-WTF flag for CSRF
+WTF_CSRF_ENABLED = True
+# Add endpoints that need to be exempt from CSRF protection
+WTF_CSRF_EXEMPT_LIST = []
+# A CSRF token that expires in 1 year
+WTF_CSRF_TIME_LIMIT = 60 * 60 * 24 * 365
+
+# Set this API key to enable Mapbox visualizations
+MAPBOX_API_KEY = 'test'
