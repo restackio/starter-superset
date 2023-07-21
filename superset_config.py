3ca4ae4ce521eebbd83f3d48cf6cffdeaa37bd4c
Oles Maiboroda <oles.maiboroda@gmail.com>
2023-07-21 16:56:56 +0200

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -1,3 +1,67 @@
+import os
+from cachelib.redis import RedisCache
+
+def env(key, default=None):
+    return os.getenv(key, default)
+
+# MAPBOX_API_KEY = env('MAPBOX_API_KEY', '')
+CACHE_CONFIG = {
+      'CACHE_TYPE': 'redis',
+      'CACHE_DEFAULT_TIMEOUT': 300,
+      'CACHE_KEY_PREFIX': 'superset_',
+      'CACHE_REDIS_HOST': env('REDIS_HOST'),
+      'CACHE_REDIS_PORT': env('REDIS_PORT'),
+      'CACHE_REDIS_PASSWORD': env('REDIS_PASSWORD'),
+      'CACHE_REDIS_DB': env('REDIS_DB', 1),
+}
+DATA_CACHE_CONFIG = CACHE_CONFIG
+
+SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{env('DB_USER')}:{env('DB_PASS')}@{env('DB_HOST')}:{env('DB_PORT')}/{env('DB_NAME')}"
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+# SECRET_KEY = env('SECRET_KEY', 'thisISaSECRET_1234')
+
+class CeleryConfig(object):
+  CELERY_IMPORTS = ('superset.sql_lab', )
+  CELERY_ANNOTATIONS = {'tasks.add': {'rate_limit': '10/s'}}
+  BROKER_URL = f"redis://{env('REDIS_HOST')}:{env('REDIS_PORT')}/0"
+  CELERY_RESULT_BACKEND = f"redis://{env('REDIS_HOST')}:{env('REDIS_PORT')}/0"
+
+CELERY_CONFIG = CeleryConfig
+RESULTS_BACKEND = RedisCache(
+      host=env('REDIS_HOST'),
+      port=env('REDIS_PORT'),
+      key_prefix='superset_results'
+)
+
+
+# Overrides
+# my_override
+FEATURE_FLAGS = {
+  "ALERTS_ATTACH_REPORTS": True,
+  "ALLOW_ADHOC_SUBQUERY": True,
+  "DASHBOARD_CROSS_FILTERS": True,
+  "DASHBOARD_RBAC": True,
+  "DISABLE_LEGACY_DATASOURCE_EDITOR": True,
+  "DRUID_JOINS": True,
+  "EMBEDDABLE_CHARTS": True,
+  "EMBEDDED_SUPERSET": True,
+  "ENABLE_DND_WITH_CLICK_UX": True,
+  "ENABLE_EXPLORE_DRAG_AND_DROP": True,
+  "ENABLE_TEMPLATE_PROCESSING": True,
+  "ENFORCE_DB_ENCRYPTION_UI": True,
+  "ESCAPE_MARKDOWN_HTML": True,
+  "LISTVIEWS_DEFAULT_CARD_VIEW": True,
+  "SCHEDULED_QUERIES": True,
+  "SQLLAB_BACKEND_PERSISTENCE": True,
+  "SQL_VALIDATORS_BY_ENGINE": True,
+  "THUMBNAILS": True,
+  "ALERT_REPORTS": True
+}
+DATA_CACHE_CONFIG = {
+  "CACHE_TYPE": "SupersetMetastoreCache",
+  "CACHE_KEY_PREFIX": "superset_results",
+  "CACHE_DEFAULT_TIMEOUT": 86400,
+}
 
 # Secret key
 SECRET_KEY= "4MCsZm1ciqjZ4347/jIrefw34vKOPZ37Rr9k2iguLd3OeeCiZ45aw5ha"
